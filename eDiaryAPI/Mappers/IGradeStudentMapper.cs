a18ac2b49711916ed6967e64d4ca4158441b3e3e
Olaf Kulawik <olaf.kulawik@gmail.com>
2021-06-16 19:34:32 +0200

    adding grade student

---
+++ eDiaryAPI/Mappers/IGradeStudentMapper.cs
@@ -0,0 +1,14 @@
+﻿using eDiaryAPI.Models.DbModels;
+using eDiaryAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDiaryAPI.Mappers
+{
+    public interface IGradeStudentMapper
+    {
+        public IList<GradeDTO> showGrade(IList<Grade> grade);
+    }
+}
